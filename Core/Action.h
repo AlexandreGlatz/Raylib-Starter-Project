b7758577c4b428468debcca555b49bde9a6933ab
Alexandre Glatz <alexandre.glatzpro@gmail.com>
2026-09-17 11:46:39 +0200

    [FIX] Actions

--- Core/Action.h
+++ Core/Action.h
@@ -2,60 +2,66 @@
 #define ACTION_H_
 
 #include <vector>
+#include <iterator>
 #include <queue>
+#include <utility>
 
-template <class R, class ... Args>
+template <typename R, typename ... Args>
 class Action
+{
+};
+
+template<typename R, typename ... Args>
+class Action<R(Args ...)>
 {
 public:
 	Action();
 	~Action();
 
-	void Subscribe(R(*func)(Args ...));
-	void SubscribeOnce(R(*func)(Args ...));
+	void Subscribe(R* (*func)(Args ...));
+	void SubscribeOnce(R* (*func)(Args ...));
 	void Execute(Args ...);
 
 private:
-	std::vector<R(*func)(Args)> m_subscribedFunctions;
-	std::queue<R(*func)(Args)> m_subscribedOnceFunctions;
+	std::vector<R* (*)(Args ...)> m_subscribedFunctions;
+	std::queue<R* (*)(Args ...)> m_subscribedOnceFunctions;
+
 };
 
-
 template<class R, class ...Args>
-inline Action<R, Args...>::Action()
+inline Action<R(Args...)>::Action()
 {
 }
 
 template<class R, class ...Args>
-inline Action<R, Args...>::~Action()
+inline Action<R(Args...)>::~Action()
 {
 }
 
 template<class R, class ...Args>
-inline void Action<R, Args...>::Subscribe(R(*func)(Args ...))
+inline void Action<R(Args...)>::Subscribe(R* (*func)(Args ...))
 {
-	m_subscribedFunctions.push(func);
+	m_subscribedFunctions.push_back(func);
 }
 
 template<class R, class ...Args>
-inline void Action<R, Args...>::SubscribeOnce(R(*func)(Args...))
+inline void Action<R(Args...)>::SubscribeOnce(R* (*func)(Args...))
 {
 	m_subscribedOnceFunctions.push(func);
 }
 
 template<class R, class ...Args>
-inline void Action<R, Args...>::Execute(Args ...)
+inline void Action<R(Args...)>::Execute(Args ... args)
 {
 	while (m_subscribedOnceFunctions.empty() == false)
 	{
-		m_subscribedOnceFunctions.front()(Args ...);
+		m_subscribedOnceFunctions.front()(std::forward<Args>(args)...);
 		m_subscribedOnceFunctions.pop();
 	}
 
-	std::vector<R(*func)(Args ...)>::iterator i;
-	for (i = m_subscribedFunctions.begin(); i < m_subscribedFunctions.end(); i++)
+	for (int i = 0; i<m_subscribedFunctions.size(); ++i)
 	{
-		*i(Args ...);
+		m_subscribedFunctions[i](std::forward<Args>(args)...);
 	}
 }
 
