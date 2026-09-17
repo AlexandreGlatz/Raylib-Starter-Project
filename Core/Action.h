52b50fb7e3a91a026a51a66612bb445415954df6
Alexandre Glatz <alexandre.glatzpro@gmail.com>
2026-09-17 10:42:25 +0200

    [REF] Action -> function pointers / queues

--- Core/Action.h
+++ Core/Action.h
@@ -1,8 +1,8 @@
 #ifndef ACTION_H_
 #define ACTION_H_
 
+#include <vector>
 #include <queue>
-#include <functional>
 
 template <class R, class ... Args>
 class Action
@@ -11,13 +11,55 @@ public:
 	Action();
 	~Action();
 
-	void Subscribe(std::function<R(Args)>);
-	void Execute(Args);
+	void Subscribe(R(*func)(Args ...));
+	void SubscribeOnce(R(*func)(Args ...));
+	void Execute(Args ...);
 
 private:
-	std::queue<std::function<R(Args)> m_functions;
-
+	std::vector<R(*func)(Args)> m_subscribedFunctions;
+	std::queue<R(*func)(Args)> m_subscribedOnceFunctions;
 };
 
+
+template<class R, class ...Args>
+inline Action<R, Args...>::Action()
+{
+}
+
+template<class R, class ...Args>
+inline Action<R, Args...>::~Action()
+{
+}
+
+template<class R, class ...Args>
+inline void Action<R, Args...>::Subscribe(R(*func)(Args ...))
+{
+	m_subscribedFunctions.push(func);
+}
+
+template<class R, class ...Args>
+inline void Action<R, Args...>::SubscribeOnce(R(*func)(Args...))
+{
+	m_subscribedOnceFunctions.push(func);
+}
+
+template<class R, class ...Args>
+inline void Action<R, Args...>::Execute(Args ...)
+{
+	while (m_subscribedOnceFunctions.empty() == false)
+	{
+		m_subscribedOnceFunctions.front()(Args ...);
+		m_subscribedOnceFunctions.pop();
+	}
+
+	std::vector<R(*func)(Args ...)>::iterator i;
+	for (i = m_subscribedFunctions.begin(); i < m_subscribedFunctions.end(); i++)
+	{
+		*i(Args ...);
+	}
+}
+
 #endif
+
+
 
