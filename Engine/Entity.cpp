326c8708135fd12d31ac6413ed3077c770224b03
Alexandre Glatz <alexandre.glatzpro@gmail.com>
2026-09-17 15:21:27 +0200

    [ADD] Entity

--- Engine/Entity.cpp
+++ Engine/Entity.cpp
@@ -1,2 +1,32 @@
 #include "pch.h"
 #include "Entity.h"
+#include "Component.h"
+
+Entity::Entity()
+{
+}
+
+Entity::~Entity()
+{
+}
+
+void Entity::Update()
+{
+	OnUpdate.Execute();
+
+	for (int i = 0; i < m_components.size(); ++i)
+	{
+		m_components[i]->Update();
+	}
+}
+
+bool Entity::IsComponentAdded(std::string type)
+{
+	auto it = std::find_if(m_components.begin(), m_components.end(), 
+		[&type](std::shared_ptr<Component> currentComponent)
+		{ return currentComponent->GetType() == type; }
+	);
+
+	return it == m_components.end();
+}
+
--- Engine/Entity.h
+++ Engine/Entity.h
@@ -5,7 +5,10 @@
 #include <vector>
 #include <memory>
 #include <optional>
+#include <type_traits>
+
 #include <Action.h>
+#include <Logger.h>
 
 class Component;
 class Entity
@@ -27,7 +30,58 @@ public:
 protected:
 	std::string m_name;
 	std::vector<Component*> m_components;
+	
+private:
+	bool IsComponentAdded(std::string type);
+	template<typename T>
+	bool IsComponent();
+
 };
 
 #endif
 
+template<typename T>
+inline std::shared_ptr<T> Entity::AddComponent()
+{
+
+	if (std::is_base_of(Component, T)::value == false)
+	{
+		Logger::Log(LOG_LEVEL::ERROR, "Can't add a non-component to an entity");
+		return nullptr;
+	}
+
+	std::shared_ptr<T> pNewComponent = std::make_shared<T>(this);
+
+	std::string newComponentType = pNewComponent.GetType();
+	if (IsComponentAdded(newComponentType))
+	{
+		Logger::LogPrintf(LOG_LEVEL::ERROR, "Component of Type %s already exists in Entity of name %s", newComponentType.c_str(), m_name.c_str());
+		return nullptr;
+	}
+
+	m_components.push_back(pNewComponent);
+	return pNewComponent;
+}
+
+template<typename T>
+inline std::optional<std::shared_ptr<T>> Entity::GetComponent() const
+{
+	if (IsComponent<T>() == false)
+	{
+		Logger::Log(LOG_LEVEL::ERROR, "Can't get a non-component class");
+		return nullptr;
+	}
+
+	if (IsComponentAdded(T::GetType()) == false)
+	{
+		Logger::LogPrintf(LOG_LEVEL::ERROR, "Component of type %s doesnt exist in entity of name %s", T::GetType().c_str(), m_name.c_str());
+	}
+
+	return std::optional<std::shared_ptr<T>>();
+}
+
+template<typename T>
+inline bool Entity::IsComponent()
+{
+	return std::is_base_of(Component, T)::value;
+}
